c92a0565dd3b29b69e2adb101c2988b15486a481
crl486 <crl486@vm-akel6416.lhup.edu>
2019-04-08 17:38:02 -0400

    Comp240 Class commit

---
+++ Comp240_Space Invaders/src/chapter2/Sprite.java
@@ -0,0 +1,47 @@
+package chapter2;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+public class Sprite {
+
+	private int frameCount;
+	private int[] src;
+	private int curr = 0;
+	private Image image;
+
+	public Sprite(String resid, int frames) {
+		frameCount = frames;
+
+		try {
+			URL url = this.getClass().getClassLoader().getResource(resid);
+			if (url == null) {
+				System.err.println("Alien Sprite not found!");
+			}
+			image = ImageIO.read(url);
+		} catch (IOException e) {
+			System.err.println("faaled to load sprite!");
+		}
+		src = new int[frameCount];
+
+		for (int i = 0; i < src.length; i++) {
+			int w = image.getWidth(null) / frameCount;
+			src[i] = i * w;
+		}
+	}
+
+	public void drawImage(Graphics2D gc, int rx, int ry, int width, int height, boolean active) {
+		int sw = image.getWidth(null) / frameCount + src[curr];
+		gc.drawImage(image, rx, ry, width, height, src[curr], 0, sw, image.getHeight(null), null);
+		if (active) {
+			curr++;
+			curr %= frameCount;
+
+		}
+
+	}
+}
